dcd196a7f8750dd470f1d7e3894c6b83d3ecde1e
Afet8888 <baku.9999@hotmail.com>
2018-04-05 18:27:23 +0400

    Fourth-fifth methods added

--- src/main/java/com/afet/Entities/User.java
+++ src/main/java/com/afet/Entities/User.java
@@ -1,52 +1,13 @@
 package com.afet.Entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class User {
     String username;
     String email;
     String password;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public User setUsername(String username) {
-        this.username = username;
-        return this;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public User setEmail(String email) {
-        this.email = email;
-        return this;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public User setPassword(String password) {
-        this.password = password;
-        return this;
-    }
-
-    public User(String username, String email, String password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
-
-    public User() {
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "username='" + username + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
--- src/main/java/com/afet/Main.java
+++ src/main/java/com/afet/Main.java
@@ -27,14 +27,24 @@ public class Main {
 //        System.out.println(Arrays.toString(string.toArray()));
 //        System.out.println(longestString(string));
 
-        List<User> users = new ArrayList<>();
-        users.add(new User("user","abvgd@mail.ru","123456"));
-        users.add(new User("user","bbvgd@mail.ru","123456"));
-        users.add(new User("user","bcbvgd@mail.ru","123456"));
-        users.add(new User("personOne","abvgd@mail.ru","123456"));
-        users.add(new User("wersonTwo","abvgd@mail.ru","123456"));
-        users.add(new User("aaaaaaa","abvgd@mail.ru","123456"));
-        System.out.println(sortByUsernameThenByEmail(users));
+//        List<User> users = new ArrayList<>();
+//        users.add(new User("user","abvgd@mail.ru","123456"));
+//        users.add(new User("user","bbvgd@mail.ru","123456"));
+//        users.add(new User("user","bcbvgd@mail.ru","123456"));
+//        users.add(new User("personOne","abvgd@mail.ru","123456"));
+//        users.add(new User("wersonTwo","abvgd@mail.ru","123456"));
+//        users.add(new User("aaaaaaa","abvgd@mail.ru","123456"));
+//        System.out.println(sortByUsernameThenByEmail(users));
+
+        Integer[] ints = {1, 2, 65, 7, 3, 6, 2, 3, 4, 8, 17};
+        System.out.println(Arrays.toString(ints));
+        ints = primeNumbersFromArrayLambdas(ints);
+        System.out.println(Arrays.toString(ints));
+
+
+
+
+
     }
 
     static Integer[] evenNumbersFromArray(Integer[] array) {
@@ -63,10 +73,34 @@ public class Main {
 //        }).get();
     }
 
-    private static List<User> sortByUsernameThenByEmail(List<User> listOfUsers){
+    private static List<User> sortByUsernameThenByEmail(List<User> listOfUsers) {
         return listOfUsers.parallelStream().
                 sorted(Comparator.comparing(User::getUsername).
-                thenComparing(User::getEmail)).collect(Collectors.toList());
+                        thenComparing(User::getEmail)).collect(Collectors.toList());
+    }
+
+    private static Integer[] primeNumbersFromArray(Integer[] array) {
+        List<Integer> tempList = new ArrayList<>(Arrays.asList(array));
+        tempList.removeIf(item -> !isPrime(item));
+        return tempList.toArray(new Integer[tempList.size()]);
+    }
+
+    private static Integer[] primeNumbersFromArrayLambdas(Integer[] array) {
+        List<Integer> tempList = new ArrayList<>(Arrays.asList(array));
+        List<Integer> newList = tempList.parallelStream().
+                filter(Main::isPrime).
+                collect(Collectors.toList());
+        return newList.
+                toArray(new Integer[newList.size()]);
+    }
+
+    private static boolean isPrime(Integer num) {
+        if(num<0)num*=-1;
+        if(num == 1)return false;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
     }
 
 
